8b823c8c1fe2d07874f85b90f8bffafef394268a
Furkan Bora Murat <muratfurkan52@gmail.com>
2023-12-21 10:44:43 +0300

    changed project name

--- 10 Minutes to Sunrise/src/main/GameWindow.java
+++ 10 Minutes to Sunrise/src/main/GameWindow.java
@@ -4,16 +4,16 @@ import javax.swing.JFrame;
 
 public class GameWindow {
 	private JFrame jframe;
+
 	public GameWindow(GamePanel gamePanel) {
-		
+
 		jframe = new JFrame();
-		jframe.setSize(600,900);
-		
+		jframe.setSize(600, 900);
+
 		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		jframe.add(gamePanel);
-		jframe.setLocationRelativeTo(null);		// Starts game in center of screen
-		
-		
-		jframe.setVisible(true); 	// Should be on bottom to draw when everything is ready
+		jframe.setLocationRelativeTo(null); // Starts game in center of screen
+
+		jframe.setVisible(true); // Should be on bottom to draw when everything is ready
 	}
 }
